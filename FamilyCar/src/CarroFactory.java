17b25549f122f7fcd338eab53eb7a2d447cbe998
Guilherme Wolner <gwdm@a.recife.ifpe.edu.br>
2020-06-03 15:50:01 -0300

    Add MontadoraDeCarro

--- FamilyCar/src/CarroFactory.java
+++
@@ -1,7 +0,0 @@
-
-public abstract class CarroFactory {
-	
-	public abstract Roda montarRoda();
-	
-	public abstract Som montarSom();
-}
--- FamilyCar/src/RodaSimples.java
+++
@@ -1,4 +0,0 @@
-
-public class RodaSimples extends Roda{
-
-}
